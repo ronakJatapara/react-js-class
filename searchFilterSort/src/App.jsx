1573dc778dd907c2613e19ca6e226098b7c0323f
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-02-14 14:31:39 +0530

    Added

---
+++ searchFilterSort/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Search from './components/search'
+import Filter from './components/filter'
+import Sorted from './components/Sorted'
+
+function App() {
+
+
+
+
+
+
+
+  return (
+<>
+
+ <Search></Search>
+ {/* <Filter></Filter> */}
+ {/* <Sorted></Sorted> */}
+ 
+</>
+  )
+}
+
+export default App
