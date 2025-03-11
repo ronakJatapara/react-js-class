381fc9049f77dbb19211425add1e616337bbb4e3
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-03-11 17:47:50 +0530

    Added

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function About() {
+
+ const info = [ 
+  {text:'Fresher in work' , count : "00"},
+  {text:'Complate Project' , count : '24'},
+  {text:'Companies Work' , count : "00"}
+
+ ]
+
   return (
     <>
-      <h1 className='text-white'>about</h1>
+      <section id='about' className='py-10 text-white' style={{height:"800px"}}>
+        <div className='text-center mt-8'>
+          <h3 className='text-4xl font-semibold'>
+            <br />
+            About <span className='text-cyan-600'>Me</span>
+          </h3>
+          <p>My Introduction</p>
+          <div className='flex md:flex-row flex-col-reverse items-center md:gap gap-12 px-10 max-w-6xl mx-auto'>
+            <div>
+              <div className='text-gray-300 my-3'>
+                <p className='text-justify leading-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, accusantium minima quos repudiandae magni ipsa tempore libero cum ducimus a maxime quibusdam illum recusandae expedita necessitatibus id, sed laboriosam, magnam autem saepe minus. Voluptates, iure. Porro blanditiis, molestias voluptates atque aspernatur ea! Quibusdam nobis assumenda nemo nesciunt minima quia, laboriosam officiis ab tempora commodi. Voluptatem ?</p>
+                <br /><br />
+                 <div className='flex mt-10 items-center gap-20'>
+                {
+                  info.map(content=>(
+                    <div key={content.text}>
+                       <h3 className='md:text-4xl text-2xl font-semibold text-white' >
+                        {content.count}
+                        <span className='text-cyan-600'>+</span>{" "}
+                       </h3>
+                       <span>{content.text}</span>
+                    </div>
+                  ))
+                }
+               
+                 </div>
+                  <br />
+                  <br />
+
+                  <Link href="#" className="flex justify-center md:justify-start">
+    <button 
+        className="rounded-full w-[180px] h-[45px] bg-[#0092B8] text-white font-semibold"
+        id="btn2"
+    >
+        Download CV
+    </button>
+</Link>
+
+
+
+              </div>
+            </div>
+
+            <div className='flex-1 flex justify-center items-center'>
+                 <div className='lg:w-96 h-full relative sm:w-10/12 max-w-sm aboutImg' style={{height:"500px" }}>
+                  <img src='download3.webp' alt="" className='w-full object-cover bg-cyan-600 rounded' style={{height:"500px", width:"100%"}}/>
+                 </div>
+
+            </div>
+
+          </div>
+        </div>
+      </section>
     </>
   )
 }
--- portfolio/src/components/Skill.jsx
+++ portfolio/src/components/Skill.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 
 function Skill() {
+
+ const skills = [
+  {logo:"logo-html5" , level:"Advance" , count:86},
+  {logo:"logo-css3" , level:"Expert" , count:90},
+  {logo:"logo-nodejs" , level:"Beginner" , count:40},
+  {logo:"logo-react" , level:"Intermedia" , count:80}
+
+
+ ]
+
   return (
     <>
-      <h1 className='text-white'>skill</h1>
+    <section id='skills'className='py-10 bg-gray-800 relative ' style={{height:"800px"}}>
+    <div className='mt-8 text-gra-100 text-center'>
+      <br /><br />
+      <h3 className='text-4xl font-semibold'>My <span className='text-cyan-600'>Skills</span></h3>
+      <p className='text-gray-400 mt-3 text-lg'>My Knowledge</p>
+      <div className='flex items-center justify-center mt-12 gap-10  flex-wrap' >
+    {
+      skills?.map((skill,i)=>(
+        <div key={i} className='border-2 border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl' >
+        <div className='w-32 h-32 flex items-center justify-center rounded-full' style={{background:`conic-gradient(rgb(8,145,170) ${skill.count}%,#ddd ${skill.count}%)`}}>
+              <div className='text-6xl text-white w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600'>
+              <ion-icon name={skill.logo}></ion-icon>
+              </div>
+        </div>
+        <p className='text-white'>{skill.level}</p>
+</div>
+      ))
+    }
+      </div>
+    </div>
+    </section>
+
+
+    <section id='hireme' className='py-10 px-3 text-white'>
+     <div className='text-center'>
+        <h3 className='text-4xl font-semibold'>
+            Hire <span className='text-cyan-600'> Me</span>
+        </h3>
+        <p className='text-gray-400 mt-3 text-lg'>Do You have Any works?</p>
+     </div>
+     <div className='bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center'>
+   <div>
+    <h3 className='text-2xl font-semibold'> Do you want any work from me?</h3>
+    <p className='max-w-lg text-sm mt-4 text-gray-200 leading-6'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus est itaque minima omnis ullam quasi excepturi deleniti tenetur consectetur illo facere, debitis officia praesentium ratione adipisci assumenda autem eum harum.</p>
+    <button className='btn-primary  mt-10 rounded-full' id='btn3' style={{height:"50px" , width:"150px", backgroundColor:"#0092B8"}}>Say Hello</button>
+   </div>
+   <img src="download3.png" alt="" className='lg:h-[32rem] h-80 lg:absolute bottom-0 -right-3 object-cover'/>
+     </div>
+       </section> 
     </>
   )
 }
