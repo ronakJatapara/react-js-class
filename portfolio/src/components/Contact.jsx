7d8b8898f69686f257c1b6f01e58ed87586f56aa
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-03-13 14:35:44 +0530

    Added

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 
 function Contact() {
+    
+  const contact_info=[
+    {logo:"mail", text:"codeprogram@gmail.com"},
+    {logo:"logo-whatsapp", text:"123 456 780"},
+    {logo:"location", text:"demo location"}
+
+
+  ]
+
   return (
     <>
-      <h1 className='text-white'>contact</h1>
+      <section id='contact' className='py-10 px-3 text-white'>
+          <div className='text-center mt-8'>
+              <h3 className='text-4xl font-semibold'>Contact<span className='text-cyan-600'>Me</span></h3>
+              <p className='text-gray-400 mt-3 text-lg'>Get in touch</p>
+
+              <div className='mt-16 flex md:flex-row flex-col gap-6 max-w-5xl  bg-gray-800 md:p-6 rounded-lg mx-auto '  >
+                <form action="" className='flex flex-col flex-1 gap-5' >
+                  <input type="text" name="" id="" placeholder='Your Name'  className="ps-2" />
+                  <input type="email" name="" id="" placeholder='Your Email Address'  className="ps-2" />
+                  <textarea name="" placeholder='Your Message' rows={10} id="" className="ps-2"  ></textarea>
+                  <button className='btn-primary rounded-full w-fit'  style={{backgroundColor:"#0092B8" , height:"40px"  , width:"150px"}}>Send Message</button>
+
+                </form>
+                <div className='flex flex-col gap-7'>
+               
+{
+  contact_info.map((contact, i) => (
+    <div key={i} className="flex flex-wrap gap-4 w-fit items-center">
+      <div className="w-14 h-14 flex items-center justify-center text-white bg-cyan-600 rounded-full">
+        <ion-icon name={contact.logo} className="text-2xl"></ion-icon>
+      </div>
+      <p className="text-base ">{contact.text}</p>
+    </div>
+  ))
+}
+
+                </div>
+              </div>
+
+          </div>
+      </section>
     </>
   )
 }
--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -7,6 +7,15 @@ import 'swiper/css/pagination';
 
 
 function Project() {
+
+const projects = [
+  {img:'pr-1.png',name:"jewellery", github_link:'https://github.com/ronakJatapara/react-js-class/tree/main/w-5' , live_link:'file:///C:/Users/ronak/OneDrive/Desktop/coding/react-js-class/w-5/index.html'},
+  {img:'pr-2.png',name:"weapons", github_link:'https://github.com/ronakJatapara/react-js-class/tree/main/w-6' , live_link:'file:///C:/Users/ronak/OneDrive/Desktop/coding/react-js-class/w-6/index.html'},
+  {img:'pr-3.png',name:"purses", github_link:'https://github.com/ronakJatapara/react-js-class/tree/main/w-14' , live_link:'file:///C:/Users/ronak/OneDrive/Desktop/coding/react-js-class/w-14/index.html'},
+
+]
+
+
   return (
     <>
     
@@ -37,86 +46,57 @@ function Project() {
   pagination={{ clickable: true }}
   modules={[Pagination, Autoplay]}
 >
-  <SwiperSlide>
-    <div className="h-[280px] w-[250px] p-4 bg-[#2c3e50] rounded-lg shadow-md flex flex-col">
-      {/* Image Wrapper for Scrolling Effect */}
-      <div className="h-[150px] w-full overflow-hidden rounded-md relative">
-        <img 
-          src="pr-1.png" 
-          alt="Project 1" 
-          className="w-full h-auto object-cover transition-transform duration-[5000ms] ease-in-out hover:translate-y-[-100%]" 
-        />
+{
+    projects.map((project_info,i)=>(
+      <SwiperSlide>
+      <div className="h-[280px] w-[250px] p-4 bg-[#2c3e50] rounded-lg shadow-md flex flex-col">
+      
+        <div className="h-[150px] w-full overflow-hidden rounded-md relative">
+          <img 
+            src={project_info.img}
+            alt="Project 1" 
+            className="w-full h-auto object-cover transition-transform duration-[5000ms] ease-in-out hover:translate-y-[-100%]" 
+          />
+        </div>
+        
+        <h3 className="text-lg text-white mt-3 font-semibold">{project_info.name}</h3>
+        
+        <div className="flex gap-2 mt-2">
+          {/* GitHub Link */}
+          <a
+            className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm"
+            href={project_info.github_link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+    
+          {/* Live Demo Link */}
+          <a
+            className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm"
+            href="http://127.0.0.1:5500/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Live Demo
+          </a>
+        </div>
+        
       </div>
-      <h3 className="text-lg text-white mt-3 font-semibold">Jewellery</h3>
-      <div className="flex gap-2 mt-2">
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Github</Link>
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Live Demo</Link>
-      </div>
-    </div>
-  </SwiperSlide>
-
-  <SwiperSlide>
-    <div className="h-[280px] w-[250px] p-4 bg-[#2c3e50] rounded-lg shadow-md flex flex-col">
-      {/* Image Wrapper for Scrolling Effect */}
-      <div className="h-[150px] w-full overflow-hidden rounded-md relative">
-        <img 
-          src="pr-2.png" 
-          alt="Project 1" 
-          className="w-full h-auto object-cover transition-transform duration-[5000ms] ease-in-out hover:translate-y-[-100%]" 
-        />
-      </div>
-      <h3 className="text-lg text-white mt-3 font-semibold">Jewellery</h3>
-      <div className="flex gap-2 mt-2">
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Github</Link>
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Live Demo</Link>
-      </div>
-    </div>
-  </SwiperSlide>
-
-
-  <SwiperSlide>
-    <div className="h-[280px] w-[250px] p-4 bg-[#2c3e50] rounded-lg shadow-md flex flex-col">
-      {/* Image Wrapper for Scrolling Effect */}
-      <div className="h-[150px] w-full overflow-hidden rounded-md relative">
-        <img 
-          src="pr-3.png" 
-          alt="Project 1" 
-          className="w-full h-auto object-cover transition-transform duration-[5000ms] ease-in-out hover:translate-y-[-100%]" 
-        />
-      </div>
-      <h3 className="text-lg text-white mt-3 font-semibold">Jewellery</h3>
-      <div className="flex gap-2 mt-2">
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Github</Link>
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Live Demo</Link>
-      </div>
-    </div>
-  </SwiperSlide>
+    </SwiperSlide>
+    
+  
+  
+    )) 
+}
 
 
-  <SwiperSlide>
-    <div className="h-[280px] w-[250px] p-4 bg-[#2c3e50] rounded-lg shadow-md flex flex-col">
-      {/* Image Wrapper for Scrolling Effect */}
-      <div className="h-[150px] w-full overflow-hidden rounded-md relative">
-        <img 
-          src="pr-3.png" 
-          alt="Project 1" 
-          className="w-full h-auto object-cover transition-transform duration-[5000ms] ease-in-out hover:translate-y-[-100%]" 
-        />
-      </div>
-      <h3 className="text-lg text-white mt-3 font-semibold">Jewellery</h3>
-      <div className="flex gap-2 mt-2">
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Github</Link>
-        <Link className="text-cyan-600 bg-gray-900 px-3 py-1 rounded-md text-sm">Live Demo</Link>
-      </div>
-    </div>
-  </SwiperSlide>
-
- 
 </Swiper>
       </div>
 
-      <div>
-        
+      <div className='lg:block hidden'>
+        <img src="download(3).png" alt=""  style={{height:"400px"}}/>
       </div>
      </div>
 
