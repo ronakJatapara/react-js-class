023468a13d168dfee38e2391e30ffe708460645f
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-05-08 12:07:35 +0530

    Added

--- nextBuy/nextBuy/src/pages/home.jsx
+++ nextBuy/nextBuy/src/pages/home.jsx
@@ -39,10 +39,60 @@ function Home() {
     return state.SliceReducer
   })
 
-  const [visibleProducts,setVisibleProducts] = useState(4)
+
+const [selectedColors, setSelectedColors] = useState({});
+
+const getSelectColor = (el,i)=>{
+    const selectedColor = selectedColors[i]
+    switch(selectedColor)
+    {
+      case "Pink":
+        return el.image;
+
+      case "Black":
+        return el.blackImage;
+
+      case "Red":
+        return el.redImage;
+       
+      case "Yellow":
+        return el.yellowImage;
+        
+        case "White":
+          return el.whiteImage;
+  
+        case "Orange":
+          return el.orangeImage;
+  
+        case "Brandy Punch":
+          return el.brandyImage;
+
+          default:
+            return el.image
+    }
+}
+
+ const [dressType,setDressType] = useState({})
+
+ const getDressType = (el,i)=>{
+    const Type = dressType[i]
 
 
+    switch(Type)
+    {
+      case "Horizon":
+        return el.image;
+        case "BoysenBerry":
+        return el.boysenberry;
+        
+        case "Indian Yellow":
+        return el.indianYellow;
 
+        default:
+          return el.image
+        
+    }
+ }
 
   return (
     <>
@@ -216,7 +266,11 @@ function Home() {
     return (
       <div key={i} className="col-span-3  p-2 rounded shadow text-center" id='WomenSectionDetails'>
         <div className="womenSectionImage">
-        <img src={el.image} alt={el.name} className="w-full h-40 object-cover " />
+        {el.mainData && <img src={el.image} alt="" className="w-full h-40 object-cover" />}
+
+        {el.moreData &&    <img src={getSelectColor(el,i)} alt="" className="w-full h-40 object-cover"/>}
+         
+        {el.moreData2 && <img src={getDressType(el,i)} alt="" className="w-full h-40 object-cover" /> } 
         <button id='imgBtn1'><i class="fa-regular fa-heart"></i>
         
         
@@ -233,17 +287,38 @@ function Home() {
        <div className=" flex justify-center gap-4 " style={{marginTop:"10px"}}>
        <font id="price1" className="" style={{fontSize:"18px"}} >{el.rs}</font><font id="price2" style={{paddingTop:"2px",fontSize:"15px",textDecoration:"line-through"}} >{el.ors}</font>
        </div>
+       {el.moreData &&
+  <select name=""  onChange={(e)=>setSelectedColors({[i]:e.target.value})} className='border border-gray-200 rounded'  id="select" >
+  <option value="Pink">Pink</option>
+  <option value="Black">Black</option>
+  <option value="Red">Red</option>
+  <option value="Yellow">Yellow</option>
+  <option value="White">White</option>
+  <option value="Orange">Orange</option>
+  <option value="Brandy Punch">Brandy Punch</option>
+</select>
+    
+}
+
+{el.moreData2 && <select name='' onChange={(e)=> setDressType({[i]:e.target.value}) } className='border border-gray-200 rounded' id="select">
+  <option value="Horizon">Horizon</option>
+  <option value="BoysenBerry">Boysenberry</option>
+  <option value="Indian Yellow">Indian Yellow</option>
+
+</select> }
+
+
 <div className="inStock flex justify-center gap-1 " style={{paddingTop:"10px"}}>
 <font><i class="fa-solid fa-circle" style={{fontSize:"6px",color:"#479043"}}></i></font><font id="stock"  style={{paddingTop:"1px",color:"#479043"}}>{el.in}</font>
 
 </div>
+
 
         </div>
       </div>
     );
   })}
 </div>
-
 
 
 
@@ -265,139 +340,6 @@ function Home() {
 }
 
 export default Home;
-
-
-
-
-
-// import React, { useRef, useEffect, useState } from 'react';
-// import Navbar from '../components/navbar';
-// import Navbar2 from '../components/navbar2';
-// import "../style/home.css";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchApi } from '../feature/CrudSlice';
-
-// function Home() {
-//   const sliderRef = useRef();
-
-//   // State to control how many products are visible
-//   const [visibleProducts, setVisibleProducts] = useState(4);
-
-//   // Auto-scroll effect for the slider
-//   useEffect(() => {
-//     const slider = sliderRef.current;
-//     if (!slider) return;
-
-//     const interval = setInterval(() => {
-//       const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
-
-//       if (Math.round(slider.scrollLeft) >= Math.round(maxScrollLeft)) {
-//         // If at end, scroll back to start
-//         slider.scrollTo({ left: 0, behavior: 'smooth' });
-//       } else {
-//         // Scroll forward
-//         slider.scrollBy({ left: 300, behavior: 'smooth' });
-//       }
-//     }, 3000); // every 3 seconds
-
-//     return () => clearInterval(interval); // cleanup on unmount
-//   }, []);
-
-//   let dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(fetchApi());
-//   }, [dispatch]);
-
-//   const record = useSelector((state) => state.SliceReducer);
-
-//   // Function to handle "See More" button click
-//   const handleSeeMore = () => {
-//     setVisibleProducts(visibleProducts + 4);
-//   };
-
-//   return (
-//     <>
-//       <Navbar />
-//       <Navbar2 />
-
-//       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-
-//       <div className="grid grid-cols-12">
-//         <div className="col-span-5">
-//           {/* Your first grid code here */}
-//         </div>
-
-//         <div className="col-span-5"></div>
-//       </div>
-
-//       <br /><br /><br /><br /><br /><br />
-
-//       <div className="relative px-4 py-4">
-//         {/* Scroll buttons */}
-//         <button
-//           onClick={() => sliderRef.current.scrollBy({ left: -300, behavior: 'smooth' })}
-//           className="absolute left-0 top-1/2 -translate-y-1/2 z-10 bg-white shadow p-2 rounded-full"
-//         >
-//           prev
-//         </button>
-
-//         <button
-//           onClick={() => sliderRef.current.scrollBy({ left: 300, behavior: 'smooth' })}
-//           className="absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-white shadow p-2 rounded-full"
-//         >
-//           next
-//         </button>
-
-//         {/* Slider content */}
-//         <div
-//           className="overflow-x-auto whitespace-nowrap scroll-smooth scrollbar-hide"
-//           ref={sliderRef}
-//         >
-//           {/* Slider content here */}
-//         </div>
-//       </div>
-//       <br /><br /><br />
-//       <h2 className="text-4xl text-center">Latest Fashion in Motion</h2>
-//       <br /><br /><br />
-
-//       <div className="womenSection">
-//         <div className="grid grid-cols-12 grid place-items-center min-h-screen">
-//           {record.data.slice(0, visibleProducts).map((el, i) => {
-//             return (
-//               <div key={i} className="col-span-3 p-2 rounded shadow text-center" id="WomenSectionDetails">
-//                 <div className="womenSectionImage">
-//                   <img src={el.image} alt={el.name} className="w-full h-40 object-cover" />
-//                   <button id="imgBtn1"><i className="fa-regular fa-heart"></i></button>
-//                   <button id="imgBtn2"><i className="fa-solid fa-eye"></i></button>
-//                   <button id="imgBtn3" className="rounded">Quick Add</button>
-//                   <button id="imgBtn4">{el.dis}</button>
-//                 </div>
-//                 <div className="womenSectionAllText">
-//                   <p className="mt-2 text-bold" id="title">{el.name}</p>
-//                   <div className="flex justify-center gap-4" style={{ marginTop: "10px" }}>
-//                     <font id="price1" className="" style={{ fontSize: "18px" }}>{el.rs}</font>
-//                     <font id="price2" style={{ paddingTop: "2px", fontSize: "15px", textDecoration: "line-through" }}>{el.ors}</font>
-//                   </div>
-//                   <div className="inStock flex justify-center gap-1" style={{ paddingTop: "10px" }}>
-//                     <font><i className="fa-solid fa-circle" style={{ fontSize: "6px", color: "#479043" }}></i></font>
-//                     <font id="stock" style={{ paddingTop: "1px", color: "#479043" }}>{el.in}</font>
-//                   </div>
-//                 </div>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       </div>
-
-//       {/* See More Button */}
-//       <div className="seeMore flex justify-center">
-//         <button id="seeMore" className="rounded" onClick={handleSeeMore}>See More Products</button>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Home;
 
 
 
